748e4d33a96b6e300cd2a266e6f89878fb1f1fb4
32comma <louis.develog@gmail.com>
2021-04-29 14:56:47 +0900

    update

---
+++ piano.js
@@ -0,0 +1,21 @@
+var sequence = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'q', 'w', 'e', 'r', 't', 'y', 'u',
+    'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n',
+    'm',
+];
+var code = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+
+var mapper = new Map();
+var idx = 0;
+var o = 1;
+
+for (var i = 0; i < sequence.length; i++) {
+    j = i % 7;
+    if (j == 0) {
+        o++;
+    }
+    mapper.set(sequence[i], code[j] + o);
+}
+document.body.addEventListener('keydown', function (e) {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease(mapper.get(e.key), "16n");
+})
